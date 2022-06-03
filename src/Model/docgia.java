69af00099390803143a9b4331d27cd75d8450951
hoanhtin1703 <84175222+hoanhtin1703@users.noreply.github.com>
2022-06-04 00:47:25 +0700

    Initial commit

---
+++ src/Model/docgia.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author admin
+ */
+public class docgia {
+    private int madocgia_22;
+    private String tendocgia_22,sdt_22,email_22,diachi_22;
+    public docgia(){}
+
+    public docgia(int madocgia_22, String tendocgia_22, String sdt_22, String email_22, String diachi_22) {
+        this.madocgia_22 = madocgia_22;
+        this.tendocgia_22 = tendocgia_22;
+        this.sdt_22 = sdt_22;
+        this.email_22 = email_22;
+        this.diachi_22 = diachi_22;
+    }
+
+    public docgia(String tendocgia_22, String sdt_22, String email_22, String diachi_22) {
+        this.tendocgia_22 = tendocgia_22;
+        this.sdt_22 = sdt_22;
+        this.email_22 = email_22;
+        this.diachi_22 = diachi_22;
+    }
+
+    public int getMadocgia_22() {
+        return madocgia_22;
+    }
+
+    public void setMadocgia_22(int madocgia_22) {
+        this.madocgia_22 = madocgia_22;
+    }
+
+    public String getTendocgia_22() {
+        return tendocgia_22;
+    }
+
+    public void setTendocgia_22(String tendocgia_22) {
+        this.tendocgia_22 = tendocgia_22;
+    }
+
+    public String getSdt_22() {
+        return sdt_22;
+    }
+
+    public void setSdt_22(String sdt_22) {
+        this.sdt_22 = sdt_22;
+    }
+
+    public String getEmail_22() {
+        return email_22;
+    }
+
+    public void setEmail_22(String email_22) {
+        this.email_22 = email_22;
+    }
+
+    public String getDiachi_22() {
+        return diachi_22;
+    }
+
+    public void setDiachi_22(String diachi_22) {
+        this.diachi_22 = diachi_22;
+    }
+
+    
+    
+}
